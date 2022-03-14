6392f523f2e9c0624cade0fd856c7e1e41017d84
Niko <63638951+NikoGham@users.noreply.github.com>
2022-03-14 20:14:59 +0000

    prepare for deploy

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const connectDB = require('./config/db')
-
+const path = require('path')
 const app = express()
 
 // Connect Database
@@ -9,12 +9,19 @@ connectDB()
 //init middleware
 app.use(express.json({ extended: false }))
 
-app.get('/', (req, res) => res.send({ msg: 'welcome to the Eldia API' }))
-
 //Define routes
 app.use('/api/players', require('./routes/players'))
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/items', require('./routes/items'))
+
+// serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'))
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+	})
+}
 
 const PORT = process.env.PORT || 5000
 
