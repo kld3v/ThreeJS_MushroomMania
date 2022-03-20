3a60763a4b70bfd6cdad0f6dbd1a613d7b42d80e
Niko <63638951+NikoGham@users.noreply.github.com>
2022-03-20 14:07:57 +0000

    edit home

--- client/src/components/loginScreen-components/pages/Home.js
+++ client/src/components/loginScreen-components/pages/Home.js
@@ -5,6 +5,8 @@ import AuthContext from '../../../context/auth/authContext.js'
 import { Navigate } from 'react-router-dom'
 
 const Home = () => {
+	const authContext = useContext(AuthContext)
+	const { isAuthenticated } = authContext
 	if (!isAuthenticated) return <Navigate to='/login' />
 
 	const experience = new Experience(document.querySelector('canvas.webgl'))
