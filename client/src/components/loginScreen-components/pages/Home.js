9f284f68fa9a3b7cb305b77423d32fb0a1262688
Niko <63638951+NikoGham@users.noreply.github.com>
2022-03-20 14:07:15 +0000

    edit home

--- client/src/components/loginScreen-components/pages/Home.js
+++ client/src/components/loginScreen-components/pages/Home.js
@@ -1,8 +1,12 @@
 import React from 'react'
 // import Inventory from '../../inventory/Inventory.js'
 import Experience from '../../../Experience/Experience.js'
+import AuthContext from '../../../context/auth/authContext.js'
+import { Navigate } from 'react-router-dom'
 
 const Home = () => {
+	if (!isAuthenticated) return <Navigate to='/login' />
+
 	const experience = new Experience(document.querySelector('canvas.webgl'))
 	return <div></div>
 }
