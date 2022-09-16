d58ade301de913fbfb9f6b685b45646e78115cfb
Niko <63638951+NikoGham@users.noreply.github.com>
2022-09-16 12:43:30 +0100

    edit server.js

--- server.js
+++ server.js
@@ -16,13 +16,12 @@ app.use('/api/items', require('./routes/items'))
 app.use('/api/stats', require('./routes/stats'))
 
 // serve static assets in production
-if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('client/build'))
 
-	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-	})
-}
+app.use(express.static('client/build'))
+
+app.get('*', (req, res) => {
+	res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+})
 
 const PORT = process.env.PORT || 5000
 
